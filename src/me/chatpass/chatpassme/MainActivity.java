66eabc30879d0b78247dcc8238a2ab4da60a756d
di <daedalusinfinity@gmail.com>
2013-07-17 21:28:38 -0700

    Added search?

--- src/me/chatpass/chatpassme/MainActivity.java
+++ src/me/chatpass/chatpassme/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.DialogFragment;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
